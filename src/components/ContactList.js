fa67b4c9258f05638b41590e07312604ccbc618b
LAPTOP-KHVCT5OL\tanyu <tanyutao913@gmail.com>
2022-10-13 13:39:53 +0800

    merge with previous commit

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -3,36 +3,6 @@ import axios from 'axios';
 
 const ContactList = () => {
   const [contacts, setContacts] = useState(null);
-<<<<<<< Updated upstream
-
-  const getQuestion = async () => {
-    await axios
-      .get(
-        'http://ototbwebapp-env-2.eba-h8xavamx.ap-southeast-1.elasticbeanstalk.com/api/contacts'
-      )
-      .then((res) => {
-        const contacts = res.data.data;
-        setContacts(contacts);
-      });
-  };
-
-  useEffect(() => {
-    getQuestion();
-  }, []);
-
-  const handleDelete = async (id) => {
-    const newContacts = contacts.filter((contact) => contact._id !== id);
-    axios
-      .delete(
-        'http://ototbwebapp-env-2.eba-h8xavamx.ap-southeast-1.elasticbeanstalk.com/api/contacts/' +
-          id
-      )
-      .then((res) => {
-        if (res.status === 200) {
-          alert('Post deleted!');
-          setContacts(newContacts);
-        }
-=======
   
   const getQuestion = async (signal) => {
     await axios.get(
@@ -53,33 +23,33 @@ const ContactList = () => {
   }, [])
 
   const handleDelete = async (id) => {
+    const newContacts = contacts.filter((contact) => contact._id !== id);
       axios
       .delete('http://ototbwebapp-env-2.eba-h8xavamx.ap-southeast-1.elasticbeanstalk.com/api/contacts/'+ id)
-      .then(() => {
-        alert("Post deleted!");
+      .then((res) => {
+        if (res.status === 200) {
+          alert('Post deleted!');
+          setContacts(newContacts);
+        }
       }).catch(err=> {
         console.log(err.message);
->>>>>>> Stashed changes
       });
-  };
+  }
 
-  if (!contacts) return null;
+  if(!contacts)
+    return null;
 
   return (
     <div className="contactList">
-      {contacts.map((contact) => (
-        <div className="contact-preview" key={contact._id}>
+        {contacts.map((contact) => (
+          <div className="contact-preview" key = {contact._id}> 
           <h2>{contact.name}</h2>
-          <p>
-            {contact.email} {contact.phone}
-          </p>
-          <button onClick={() => handleDelete(contact._id)}>
-            delete contact
-          </button>
-        </div>
-      ))}
-    </div>
+          <p>{contact.email} {contact.phone}</p>
+          <button onClick={() => handleDelete(contact._id)}>delete contact</button>
+          </div>
+        ))}
+      </div>
   );
-};
-
+}
+ 
 export default ContactList;
