347d100954b63af1d62b6106d44fcb29795dea33
Tan Yu Tao <tanyutao913@gmail.com>
2022-10-16 13:51:54 +0800

    add navigation to home after create/patch

--- src/components/ContactDetails.js
+++ src/components/ContactDetails.js
@@ -1,88 +1,96 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
-import axios from "axios";
+import axios from 'axios';
 
 const ContactDetails = () => {
-    const { id } = useParams()
-    const [contact, setContact] = useState(null);
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [phone, setPhone] = useState('');
-    const [gender, setGender] = useState(''); 
-    
-    const getQuestion = async (signal) => {
-        await axios.get(
-            'http://ototbwebapp-env-2.eba-h8xavamx.ap-southeast-1.elasticbeanstalk.com/api/contacts/'+id, signal
-          ).then((res) => {
-            const contact = res.data.data;
-            setContact(contact);
-            setName(contact.name);
-            setEmail(contact.email);
-            setGender(contact.gender);
-            contact.phone && setPhone(contact.phone);
-          }).catch(err=> {
-              console.log(err.message);
-          })
-    }
+  const { id } = useParams();
+  const [contact, setContact] = useState(null);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [gender, setGender] = useState('');
+  const navigate = useNavigate();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        const contact = {name, email, phone, gender};
-        await axios.patch('http://ototbwebapp-env-2.eba-h8xavamx.ap-southeast-1.elasticbeanstalk.com/api/contacts/'+id, contact)
-        .then((res) => {
-            if (res.status === 200) {
-              alert('contact updated!');
-            } else {
-                console.log(res.data.message)
-            }
-        })
-    }
+  const getQuestion = async (signal) => {
+    await axios
+      .get(
+        'http://ototbwebapp-env-2.eba-h8xavamx.ap-southeast-1.elasticbeanstalk.com/api/contacts/' +
+          id,
+        signal
+      )
+      .then((res) => {
+        const contact = res.data.data;
+        setContact(contact);
+        setName(contact.name);
+        setEmail(contact.email);
+        setGender(contact.gender);
+        contact.phone && setPhone(contact.phone);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
 
-    useEffect(() => {
-        const abortCont = new AbortController();
-        getQuestion({signal: abortCont.signal});
-    
-        return () => abortCont.abort();
-    }, [])
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const contact = { name, email, phone, gender };
+    await axios
+      .patch(
+        'http://ototbwebapp-env-2.eba-h8xavamx.ap-southeast-1.elasticbeanstalk.com/api/contacts/' +
+          id,
+        contact
+      )
+      .then((res) => {
+        if (res.status === 200) {
+          alert('contact updated!');
+        } else {
+          console.log(res.data.message);
+        }
+        navigate('/');
+      });
+  };
 
-    if(!contact)
-    return null;
+  useEffect(() => {
+    const abortCont = new AbortController();
+    getQuestion({ signal: abortCont.signal });
 
-    return (
-        <div className="create">
-            <form onSubmit={handleSubmit}>
-                <label>Contact Name:</label>
-                <input
-                    type="text"
-                    required
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                />
-                <label>Contact email:</label>
-                <input
-                    type="text"
-                    required
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <label>Contact phone:</label>
-                <input
-                    type="text"
-                    value={phone}
-                    onChange={(e) => setPhone(e.target.value)}
-                />
-                <label>Contact gender:</label>
-                <select
-                    value = {gender}
-                    onChange={(e) => setGender(e.target.value)}
-                >
-                    <option value="male">male</option>
-                    <option value="female">female</option>
-                </select>
-                <button>Update Contact</button>
-            </form>
-        </div>
-    );
-}
- 
+    return () => abortCont.abort();
+  }, []);
+
+  if (!contact) return null;
+
+  return (
+    <div className="create">
+      <form onSubmit={handleSubmit}>
+        <label>Contact Name:</label>
+        <input
+          type="text"
+          required
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label>Contact email:</label>
+        <input
+          type="text"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label>Contact phone:</label>
+        <input
+          type="text"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+        />
+        <label>Contact gender:</label>
+        <select value={gender} onChange={(e) => setGender(e.target.value)}>
+          <option value="male">male</option>
+          <option value="female">female</option>
+        </select>
+        <button>Update Contact</button>
+      </form>
+    </div>
+  );
+};
+
 export default ContactDetails;
--- src/components/Create.js
+++ src/components/Create.js
@@ -1,59 +1,62 @@
-import axios from "axios";
-import { useState } from "react";
-
+import axios from 'axios';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 const Create = () => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [phone, setPhone] = useState('');
-    const [gender, setGender] = useState('male');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [gender, setGender] = useState('male');
+  const navigate = useNavigate();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        const contact = {name, email, phone, gender};
-        await axios.post('http://ototbwebapp-env-2.eba-h8xavamx.ap-southeast-1.elasticbeanstalk.com/api/contacts', contact)
-        .then((res) => {
-            if (res.status === 201) {
-              alert('contact created!');
-            }
-        })
-    }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const contact = { name, email, phone, gender };
+    await axios
+      .post(
+        'http://ototbwebapp-env-2.eba-h8xavamx.ap-southeast-1.elasticbeanstalk.com/api/contacts',
+        contact
+      )
+      .then((res) => {
+        if (res.status === 201) {
+          alert('contact created!');
+        }
+        navigate('/');
+      });
+  };
 
-    return (
-        <div className="create">
-            <h2>Add a new contact</h2>
-            <form onSubmit={handleSubmit}>
-                <label>Contact Name:</label>
-                <input
-                    type="text"
-                    required
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                />
-                <label>Contact email</label>
-                <input
-                    type="text"
-                    required
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <label>Contact phone</label>
-                <input
-                    type="text"
-                    value={phone}
-                    onChange={(e) => setPhone(e.target.value)}
-                />
-                <label>Contact gender</label>
-                <select
-                    value = {gender}
-                    onChange={(e) => setGender(e.target.value)}
-                >
-                    <option value="male">male</option>
-                    <option value="female">female</option>
-                </select>
-                <button>Add Contact</button>
-            </form>
-        </div>
-    );
-}
- 
+  return (
+    <div className="create">
+      <h2>Add a new contact</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Contact Name:</label>
+        <input
+          type="text"
+          required
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label>Contact email</label>
+        <input
+          type="text"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label>Contact phone</label>
+        <input
+          type="text"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+        />
+        <label>Contact gender</label>
+        <select value={gender} onChange={(e) => setGender(e.target.value)}>
+          <option value="male">male</option>
+          <option value="female">female</option>
+        </select>
+        <button>Add Contact</button>
+      </form>
+    </div>
+  );
+};
+
 export default Create;
