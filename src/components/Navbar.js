c9491b1650ccf233f893233be4ed938850c2ef19
Tan Yu Tao <tanyutao913@gmail.com>
2022-10-21 15:18:52 +0800

    includes changes to use serverless function to get weather forecast

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,59 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Navbar = () => {
-    return ( 
-        <nav className="navbar">
-            <h1>My Contacts</h1>
-            <div className="links">
-                <Link to="/">Home</Link>
-                <Link to="/create">New Contact</Link>
-            </div>
-        </nav>
-    );
-}
- 
+  const [open, setOpen] = useState(false);
+  const [weather, setWeather] = useState(null);
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
+  const getWeather = async (signal) => {
+    axios
+      .get(
+        'https://45g3yob2rb6l4jnrlnwcbcsiju0qemxk.lambda-url.ap-southeast-1.on.aws/',
+        signal
+      )
+      .then((res) => {
+        const weatherData = res.data;
+        console.log(weatherData);
+        setWeather(weatherData);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  useEffect(() => {
+    const abortCont = new AbortController();
+    getWeather({ signal: abortCont.signal });
+
+    return () => abortCont.abort();
+  }, []);
+
+  if (!weather) return null;
+
+  return (
+    <nav className="navbar">
+      <h1>My Contacts</h1>
+      <div className="links">
+        <button onClick={handleOpen}>Weather Forecast</button>
+        {open ? (
+          <div className="menu">
+            {weather.map((period) => (
+              <div className="period" key={period.time}>
+                {period.time.split('T')[1].substring(0, 5)} {period.forecast}
+              </div>
+            ))}
+          </div>
+        ) : null}
+        <Link to="/">Home</Link>
+        <Link to="/create">New Contact</Link>
+      </div>
+    </nav>
+  );
+};
+
 export default Navbar;
